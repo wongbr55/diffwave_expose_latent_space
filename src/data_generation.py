f48ffc45015bc51b33ea98954946ec00dc428909
wongbr55 <sudburymailing@gmail.com>
2025-12-12 15:45:06 -0500

    Added said changed from last commit

--- src/data_generation.py
+++ src/data_generation.py
@@ -6,6 +6,8 @@ import os
 import torch
 import numpy as np
 import json
+import wave
+from piper import PiperVoice
 from diffwave.inference import predict as diffwave_predict
 from diffwave.preprocess import preprocess_directory as mel_spectrogram_proccess
 # package required for loading data
@@ -14,7 +16,7 @@ import h5py
 # DiffWave only works with a specific sample rate
 SAMPLE_RATE = 22050
 
-def generate_audio_waveforms(sentences: list[str], output_dir: str):
+def generate_audio_waveforms(sentences: list[str], output_dir: str, tts_dir: str):
     """Generates the audio waveforms for ground truth sentences and saves them in output_dir
 
     Args:
@@ -23,23 +25,29 @@ def generate_audio_waveforms(sentences: list[str], output_dir: str):
     """
     
     os.makedirs(output_dir, exist_ok=True)
-
     for i, text in enumerate(sentences, start=1):
-        # Generate TTS in memory
-        tts = gTTS(text=text, lang="en")
-        mp3_fp = io.BytesIO()
-        tts.write_to_fp(mp3_fp)
-        mp3_fp.seek(0)  # rewind
-
-        # Load MP3 from memory with pydub
-        audio = AudioSegment.from_file(mp3_fp, format="mp3")
-
-        # Resample to 22050 Hz and export
-        audio = audio.set_frame_rate(SAMPLE_RATE)
-        path = os.path.join(output_dir, f"sentence_{i}.wav")
-        audio.export(path, format="wav")
         
-def generate_latent_and_mel(file_dir: str, save_dir: str, model_dir: str, fast_sampling=True):
+        voice = PiperVoice.load(tts_dir)
+        with wave.open(f"{output_dir}/sentence_{i}.wav", "wb") as wav_file:
+            voice.synthesize(text, wav_file)
+            
+        # BELOW is for gTTS
+        # commented out due to need for internet access
+        # Generate TTS in memory
+        # tts = gTTS(text=text, lang="en")
+        # mp3_fp = io.BytesIO()
+        # tts.write_to_fp(mp3_fp)
+        # mp3_fp.seek(0)  # rewind
+
+        # # Load MP3 from memory with pydub
+        # audio = AudioSegment.from_file(mp3_fp, format="mp3")
+
+        # # Resample to 22050 Hz and export
+        # audio = audio.set_frame_rate(SAMPLE_RATE)
+        # path = os.path.join(output_dir, f"sentence_{i}.wav")
+        # audio.export(path, format="wav")
+        
+def generate_latent_and_mel(file_dir: str, save_dir: str, model_dir: str, tts_dir: str, fast_sampling=True):
     """Generates full dataset of latent variables and saves in save_dir
 
     :param file_dir: directory containing h5py folder
@@ -53,35 +61,37 @@ def generate_latent_and_mel(file_dir: str, save_dir: str, model_dir: str, fast_s
     """
     # collect data from file_dir
     os.makedirs(save_dir, exist_ok=True)
-    for filename in os.listdir(file_dir):
-        if filename.endswith(".hdf5"):
-            file_path = os.path.join(file_dir, filename)
-            curr_dir = f"{save_dir}/{file_path.removesuffix('.hdf5')}"
-            os.makedirs(curr_dir, exist_ok=True)
-            # load data from file
-            data = load_h5py_file(file_path)
-            # generate audio waveforms
-            generate_audio_waveforms(data["sentence_label"], curr_dir)
-            # generate mel spectrograms
-            mel_spectrogram_proccess(curr_dir)
-            # generate latent variables for each sentence
-            for i in range(0, len(data["sentence_label"])):
-                # extract latent variables
-                mel = np.load(f"{curr_dir}/sentence_{i}.spec.npy")
-                mel = torch.from_numpy(mel).float()
-                __, __, latent_vars = diffwave_predict(mel, model_dir, fast_sampling=fast_sampling, expose_latent_vars=True)
-                latent_vars_serializable = {k: v.tolist() for k, v in latent_vars.items()}
-                # extract neural features for sentence i
-                # of length (T, 512)
-                neural_features = data["neural_features"][i]
-                # save latent variables and latent variables
-                curr_sentence_data = {
-                    "latent_variables": latent_vars_serializable,
-                    "neural_features": neural_features.tolist() if isinstance(neural_features, np.ndarray) else neural_features
-                }
-                with open(os.path.join(curr_dir, f"sentence_{i}_neural_latent_data.json"), "w") as f:
-                    json.dump(curr_sentence_data, f, indent=4)
-                
+    for dir in os.listdir(file_dir):
+        for filename in os.listdir(f"{file_dir}/{dir}"):
+            if filename.endswith(".hdf5"):
+                file_path = os.path.join(f"{file_dir}/{dir}", filename)
+                curr_dir = f"{save_dir}/{dir}/{filename.removesuffix('.hdf5')}"
+                os.makedirs(curr_dir, exist_ok=True)
+                # load data from file
+                data = load_h5py_file(file_path)
+                # generate audio waveforms
+                generate_audio_waveforms(data["sentence_label"], curr_dir, tts_dir)
+                # generate mel spectrograms
+                mel_spectrogram_proccess(curr_dir)
+                # generate latent variables for each sentence
+                for i in range(0, len(data["sentence_label"])):
+                    # extract latent variables
+                    mel = np.load(f"{curr_dir}/sentence_{i+1}.wav.spec.npy")
+                    mel = torch.from_numpy(mel).float()
+                    __, __, latent_vars = diffwave_predict(mel, model_dir, fast_sampling=fast_sampling, expose_latent_vars=True)
+                    latent_vars_serializable = {k: v.tolist() for k, v in latent_vars.items()}
+                    # extract neural features for sentence i
+                    # of length (T, 512)
+                    neural_features = data["neural_features"][i]
+                    # save latent variables and latent variables
+                    curr_sentence_data = {
+                        "latent_variables": latent_vars_serializable,
+                        "neural_features": neural_features.tolist() if isinstance(neural_features, np.ndarray) else neural_features
+                    }
+                    with open(os.path.join(curr_dir, f"sentence_{i}_neural_latent_data.json"), "w") as f:
+                        json.dump(curr_sentence_data, f, indent=4)
+        
+        break
 # CODE TAKEN FROM KAGGLE TO LOAD DATA
 def load_h5py_file(file_path: str):
     data = {
@@ -129,4 +139,5 @@ def load_h5py_file(file_path: str):
 if __name__ == "__main__":
     generate_latent_and_mel("/scratch/wongbr55/brain_to_text_25/t15_copyTask_neuralData/hdf5_data_final",
                             "/scratch/wongbr55/latent_mel_data",
-                            "/home/wongbr55/diff_brain_decoding/diffwave_expose_latent_space/pretrained_model/diffwave-ljspeech-22kHz-1000578.pt")
+                            "/home/wongbr55/diff_brain_decoding/diffwave_expose_latent_space/pretrained_model/diffwave-ljspeech-22kHz-1000578.pt",
+                            "/home/wongbr55/diff_brain_decoding/diffwave_expose_latent_space/pretrained_model/en_US-amy-medium.onnx")
