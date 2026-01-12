7ff0a1e576032ca18d2eea5de57446755af63063
wongbr55 <sudburymailing@gmail.com>
2026-01-12 11:30:17 -0500

    Modified training code to have new auto-reg. inference, and save model checkpoint after every epoch

--- model_training.py
+++ model_training.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.cuda.amp import autocast, GradScaler
 import numpy as np
 from data_loading import construct_latent_dataset
 from torch.nn.utils.rnn import pad_sequence
@@ -8,6 +9,7 @@ from torch.utils.data import DataLoader
 from torch.optim import AdamW
 import matplotlib.pyplot as plt
 import pdb
+import json
 
 
 
@@ -15,214 +17,188 @@ import pdb
 # TOY MODEL
 ###################################
 
+###################################
+# POSITIONAL ENCODING
+###################################
 class SinusoidalPositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=150000):
         super().__init__()
-
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2, dtype=torch.float)
-            * (-np.log(10000.0) / d_model)
-        )
-
+        position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float32) * (-np.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-
-        pe = pe.unsqueeze(0)  # (1, max_len, d_model)
-        self.register_buffer("pe", pe)
+        self.register_buffer("pe", pe.unsqueeze(0))  # (1, max_len, d_model)
 
     def forward(self, x):
-        """
-        x: (B, T, d_model)
-        """
+        # x: (B, T, d_model)
         return x + self.pe[:, :x.size(1)]
 
+
+###################################
+# ENCODER
+###################################
 class Encoder(nn.Module):
     def __init__(self, input_dim, d_model):
         super().__init__()
         self.in_proj = nn.Linear(input_dim, d_model)
         self.pos_enc = SinusoidalPositionalEncoding(d_model)
-
         self.transformer = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(
-                d_model=d_model,
-                nhead=8,
-                batch_first=True
-            ),
-            num_layers=4
+            nn.TransformerEncoderLayer(d_model=d_model, nhead=2, batch_first=True),
+            num_layers=1
         )
 
     def forward(self, x, input_lengths):
         B, T, _ = x.shape
-        padding_mask = torch.arange(T, device=x.device)[None, :] >= input_lengths[:, None]
-
-        x = self.in_proj(x)
-        x = self.pos_enc(x)
+        # boolean mask for padding
+        padding_mask = torch.arange(T, device=x.device)[None, :] >= input_lengths.to(x.device)[:, None]
+        x = self.pos_enc(self.in_proj(x))
         memory = self.transformer(x, src_key_padding_mask=padding_mask)
-
         return memory, padding_mask
 
-
-# OLD decoder, padding/attention matricies too large
-# class Decoder(nn.Module):
-#     def __init__(self, d_model):
-#         super().__init__()
-#         self.embed = nn.Linear(1, d_model)  # scalar target → d_model
-
-#         self.transformer = nn.TransformerDecoder(
-#             nn.TransformerDecoderLayer(
-#                 d_model=d_model,
-#                 nhead=8,
-#                 batch_first=True
-#             ),
-#             num_layers=4
-#         )
-#         self.pos_enc = SinusoidalPositionalEncoding(d_model)
-
-
-#         self.out = nn.Linear(d_model, 1)     # regression output
-#         self.stop = nn.Linear(d_model, 1)    # stop logit
-
-#     def forward(self, tgt, memory, tgt_mask=None, tgt_key_padding_mask=None, memory_key_padding_mask=None):
-#         # tgt: (B, T, 1)
-#         tgt = self.embed(tgt)
-#         tgt = self.pos_enc(tgt)
-
-
-#         hidden = self.transformer(
-#             tgt=tgt,
-#             memory=memory,
-#             tgt_mask=tgt_mask,
-#             tgt_key_padding_mask=tgt_key_padding_mask,
-#             memory_key_padding_mask=memory_key_padding_mask
-#         )                                 # (B, T, d_model)
-
-#         preds = self.out(hidden).squeeze(-1)       # (B, T)
-#         stop_logits = self.stop(hidden).squeeze(-1)  # (B, T)
-
-#         return preds, stop_logits
-
-class RNNDecoder(nn.Module):
-    
-    def __init__(self, d_model) -> None:
+class RNNEncoder(nn.Module):
+    def __init__(self, input_dim, d_model):
         super().__init__()
-        self.embed = nn.Linear(1, d_model)
-        self.rnn = nn.GRU(
-            d_model,
-            d_model,
-            num_layers=4,
+        self.in_proj = nn.Linear(input_dim, d_model)
+        # self.pos_enc = SinusoidalPositionalEncoding(d_model)
+        self.rnn = nn.LSTM(d_model, d_model, num_layers=1, batch_first=True, bidirectional=False)
+    
+    def forward(self, x, input_lengths):
+        B, T, __ = x.shape
+        x = self.in_proj(x)
+        packed_x = nn.utils.rnn.pack_padded_sequence(
+            x,
+            input_lengths.cpu(),
+            batch_first=True,
+            enforce_sorted=False
+        )
+        packed_out, (h_n, c_n) = self.rnn(packed_x)
+        out, _ = nn.utils.rnn.pad_packed_sequence(
+            packed_out,
             batch_first=True
         )
+        
+        return out, (h_n, c_n)
+
+
+###################################
+# RNN DECODER
+###################################
+class RNNDecoder(nn.Module):
+    def __init__(self, d_model, num_layers=1):
+        super().__init__()
+        self.embed = nn.Linear(1, d_model)
+        self.rnn = nn.GRU(d_model, d_model, num_layers=num_layers, batch_first=True)
         self.out = nn.Linear(d_model, 1)
         self.stop = nn.Linear(d_model, 1)
-    
+
     def forward(self, tgt, target_lengths, h0):
-        # Ensure contiguous
-        tgt = tgt.contiguous()
-        h0 = h0.contiguous()
-        
-        # Embed and make contiguous
-        x = self.embed(tgt).contiguous()
+        x = self.embed(tgt)  # (B, T, d_model)
+        packed = nn.utils.rnn.pack_padded_sequence(x, target_lengths.cpu(), batch_first=True, enforce_sorted=False)
 
-        # Pack
-        packed = nn.utils.rnn.pack_padded_sequence(
-            x, target_lengths.cpu(), batch_first=True, enforce_sorted=False
-        )
+        with torch.backends.cudnn.flags(enabled=False):
+            packed_out, _ = self.rnn(packed, h0)
 
-        # Run RNN
-        packed_out, _ = self.rnn(packed, h0)
-
-        # Unpack
-        x, _ = nn.utils.rnn.pad_packed_sequence(
-            packed_out, batch_first=True
-        )
+        x, _ = nn.utils.rnn.pad_packed_sequence(packed_out, batch_first=True)
 
         preds = self.out(x).squeeze(-1)
-        stop_logits = self.stop(x).squeeze(-1)
+        stop_logits = torch.sigmoid(self.stop(x).squeeze(-1))
         return preds, stop_logits
-        
 
+
+###################################
+# FULL MODEL
+###################################
 class NeuralToLatentModel(nn.Module):
     def __init__(self, input_dim, d_model):
         super().__init__()
-        self.encoder = Encoder(input_dim, d_model)
+        self.encoder = RNNEncoder(input_dim, d_model)
         self.decoder = RNNDecoder(d_model)
-        
-        # learnt begining of seq. token
-        # since we are dealing with continous "tokens", we learn a valuable starting representation
-        self.bos = nn.Parameter(torch.zeros(1, 1, 1))
+        self.bos = nn.Parameter(torch.zeros(1, 1, 1))  # learned start token
 
-    def forward(self, inputs, input_lengths, targets, target_lengths):
+    def forward(self, inputs, input_lengths, targets=None, target_lengths=None):
         """
-        inputs:  (B, 512, input_dim)
-        targets: (B, T)   # teacher forcing
+        inputs: (B, 512, input_dim)
+        targets: (B, T)
+        """
+    
+        if targets is None:
+            audio_wavs = []
+            generated, gen_lengths = self.generate(inputs, input_lengths)
+            for i in range(B):
+                audio = generated[i, :gen_lengths[i]]
+                audio_wavs.append(audio)
+            return audio_wavs
+
+        _, (h_n, _) = self.encoder(inputs, input_lengths)
+        # h_n: (num_layers, B, d_model)
+
+        h0 = h_n.contiguous()
+        B = targets.size(0)
+        tgt_in = torch.cat(
+            [self.bos.expand(B, 1, 1), targets[:, :-1].unsqueeze(-1)],
+            dim=1
+        )
+        preds, stop_logits = self.decoder(
+            tgt_in,
+            target_lengths - 1,
+            h0
+        )
+        return preds, stop_logits
+    
+    @torch.no_grad()
+    def generate(self, inputs, input_lengths, stop_threshold=0.5, max_len=100000):
+        """Autoregressive inference
+
+        :param inputs: _description_
+        :type inputs: _type_
+        :param input_lengths: _description_
+        :type input_lengths: _type_
+        :param h0: _description_
+        :type h0: _type_
+        :param stop_threshold: _description_, defaults to 0.5
+        :type stop_threshold: float, optional
         """
         
-        # take transformer encoding and perform reduction
-        memory, memory_padding_mask = self.encoder(inputs, input_lengths)
-        B, T_enc, D = memory.shape
+        device = inputs.device
 
-        # Take final timestep of each sequence for h0
-        idx = (input_lengths - 1).view(B, 1, 1).expand(B, 1, D)
-        enc_final = memory.gather(1, idx).squeeze(1).contiguous()
-        h0 = enc_final.unsqueeze(0).repeat(self.decoder.rnn.num_layers, 1, 1).contiguous()
+        # ----- Encode -----
+        _, (h_n, _) = self.encoder(inputs, input_lengths)
+        h0 = h_n.contiguous()  # (num_layers, B, d_model)
 
-        if targets is not None:
-            B, T = targets.shape
-            bos = self.bos.expand(B, 1, 1)
+        B = inputs.size(0)
 
-            tgt_in = torch.cat([bos, targets[:, :-1].unsqueeze(-1)], dim=1).contiguous()
+        # ----- Initialize with BOS -----
+        generated = self.bos.expand(B, 1, 1).to(device)
+        lengths = torch.ones(B, dtype=torch.long, device=device)
+        finished = torch.zeros(B, dtype=torch.bool, device=device)
 
+        for __ in range(max_len):
             preds, stop_logits = self.decoder(
-                tgt=tgt_in,
-                target_lengths=target_lengths - 1,
-                h0=h0,
+                generated,
+                lengths,
+                h0
             )
 
-            return preds, stop_logits
-        
-    #     return self.generate(inputs, input_lengths, memory, memory_padding_mask)
-    
-    # # TODO change to work with RNN decoder
-    # @torch.no_grad()
-    # def generate(self, inputs, input_lengths, memory=None, memory_padding_mask=None, max_len=100000, stop_threshold=0.5):
-    #     """
-    #     Autoregressive inference, at inference we do not have access to targets so we perform autoregressive latent genereation
-    #     """
-    #     device = inputs.device
-    #     B = inputs.size(0)
+            # Last timestep outputs
+            next_val = preds[:, -1:, :]           # (B, 1, 1)
+            stop_prob = torch.sigmoid(stop_logits[:, -1])  # (B,)
 
-    #     if memory is None:
-    #         memory, memory_padding_mask = self.encoder(inputs, input_lengths)
+            generated = torch.cat([generated, next_val], dim=1)
 
-    #     # initialize with BOS
-    #     generated = self.bos.expand(B, 1, 1)  # (B, 1, 1)
-    #     stop_mask = torch.zeros(B, dtype=torch.bool, device=device)
+            # Update lengths ONLY for unfinished
+            lengths += (~finished).long()
 
-    #     for __ in range(max_len):
-    #         T = generated.size(1)
-    #         tgt_mask = nn.Transformer.generate_square_subsequent_mask(T).to(device)
-    #         preds, stop_logits = self.decoder(
-    #             tgt=generated,
-    #             memory=memory,
-    #             tgt_mask=tgt_mask,
-    #             tgt_padding_mask=None,
-    #             memory_padding_mask=memory_padding_mask
-    #         )
+            finished |= stop_prob > stop_threshold
+            if finished.all():
+                break
 
-    #         # take last timestep prediction
-    #         next_step = preds[:, -1].unsqueeze(-1)  # (B, 1)
-    #         generated = torch.cat([generated, next_step.unsqueeze(-1)], dim=1)  # (B, T+1, 1)
+        # Remove BOS
+        generated = generated[:, 1:]
+        # lengths currently include BOS → subtract 1
+        lengths = lengths - 1
 
-    #         # check stop logits
-    #         stop_prob = torch.sigmoid(stop_logits[:, -1])
-    #         stop_mask |= stop_prob > stop_threshold
-
-    #         if stop_mask.all():
-    #             break
-
-    #     return generated[:, 1:, 0]  # remove BOS, shape: (B, T_generated)
+        return generated, lengths
 
 ###################################
 # TOY MODEL
@@ -286,7 +262,12 @@ def train_loop(train_loader, model, device, optimizer, stop_loss_threshold):
     train_reg_sum = 0.0
     train_stop_sum = 0.0
     train_count = 0
+    loop_counter = 0
     for inputs, input_lengths, targets, target_lengths, stop_targets, lengths in train_loader:
+        # if loop_counter > 0:
+        #     break
+        print(f"Training loop iteration {loop_counter}")
+        loop_counter += 1
         inputs = inputs.to(device)
         input_lengths = input_lengths.to(device)
         targets = targets.to(device)
@@ -295,7 +276,6 @@ def train_loop(train_loader, model, device, optimizer, stop_loss_threshold):
         stop_targets = stop_targets.to(device)
         lengths = lengths.to(device)
         
-
         optimizer.zero_grad()
 
         preds, stop_logits = model(inputs, input_lengths, targets, target_lengths)
@@ -337,10 +317,15 @@ def val_loop(val_loader, model, device, stop_loss_threshold):
     val_loss_sum = 0.0
     val_reg_sum = 0.0
     val_stop_sum = 0.0
-    val_count = 0
-
+    val_count = 0.0
+    
+    loop_iteration = 0
     with torch.no_grad():
         for inputs, input_lengths, targets, target_lengths, stop_targets, lengths in val_loader:
+            # if loop_iteration > 0:
+            #     break
+            print(f"Val loop iteration {loop_iteration}")
+            loop_iteration += 1
             inputs = inputs.to(device)
             targets = targets.to(device)
             stop_targets = stop_targets.to(device)
@@ -382,19 +367,27 @@ def val_loop(val_loader, model, device, stop_loss_threshold):
 ###################################
 # requires at least 1 node with 4 GPUs to run
 if __name__ == "__main__":
-    # PARAMETERS
-    
-    torch.backends.cudnn.enabled = False
+    # PARAMETERS    
+    MODEL_NAME = "toy_model"
     INPUT_DIM = 512
+    d_model = 128
     num_epochs = 10
-    model = NeuralToLatentModel(INPUT_DIM, 256)
+    model = NeuralToLatentModel(INPUT_DIM, d_model)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Device: {device}")
+    # does not work for arb. len sequences
+    # model = nn.DataParallel(model)
     model = model.to(device)
+    # model = torch.compile(model, backend="aot_eager", fullgraph=False)
     optimizer = AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=5e-4)
-    stop_loss_threshold = 0.1
-    batch_size = 8
+    stop_loss_threshold = 0.5
+    batch_size = 128
     
     train_dataset, val_dataset = construct_latent_dataset("/scratch/wongbr55/latent_mel_data", 3)
+    print(f"Number of training examples: {len(train_dataset)}")
+    print(f"Number of val examples: {len(val_dataset)}")
+    print(f"Number of train loop iterations: {len(train_dataset) // batch_size}")
+    print(f"Number of val loop iterations: {len(val_dataset) // batch_size}")
     train_loader = DataLoader(
         train_dataset,
         batch_size=batch_size,
@@ -418,6 +411,8 @@ if __name__ == "__main__":
     val_stop_loss = []
     
     for epoch in range(num_epochs):
+        print(f"##############")
+        print(f"Starting epoch {epoch}")
         train_loss_sum, train_reg_sum, train_stop_sum, train_count = train_loop(train_loader, model, device, optimizer, stop_loss_threshold)
         val_loss_sum, val_reg_sum, val_stop_sum, val_count = val_loop(val_loader, model, device, stop_loss_threshold)
         
@@ -436,13 +431,25 @@ if __name__ == "__main__":
         val_total_loss.append(avg_val_loss)
         val_reg_loss.append(avg_val_reg)
         val_stop_loss.append(avg_val_stop)
-    
+        
+        torch.save({
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "train_total_loss" : train_total_loss,
+        "train_reg_loss" : train_reg_loss,
+        "train_stop_loss" : train_stop_loss,
+        "val_total_loss" : val_total_loss,
+        "val_reg_loss" : val_reg_loss,
+        "val_stop_loss" : val_stop_loss
+        }, f"/scratch/wongbr55/{MODEL_NAME}_checkpoint_epoch_{epoch}.pt")
+        
 
-    torch.save({
-    "epoch": epoch,
-    "model_state_dict": model.state_dict(),
-    "optimizer_state_dict": optimizer.state_dict(),
-    }, "/scratch/wongbr55/toy_model_checkpoint.pt")
+    # torch.save({
+    # "epoch": epoch,
+    # "model_state_dict": model.state_dict(),
+    # "optimizer_state_dict": optimizer.state_dict(),
+    # }, f"/scratch/wongbr55/{MODEL_NAME}_checkpoint.pt")
 
     epochs = range(1, len(train_total_loss) + 1)
     fig, axes = plt.subplots(1, 3, figsize=(18, 4))
@@ -474,7 +481,7 @@ if __name__ == "__main__":
     axes[2].grid(True)
 
     plt.tight_layout()
-    plt.savefig("/scratch/wongbr55/toy_model_loss_components.png", dpi=300)
+    plt.savefig(f"/scratch/wongbr55/{MODEL_NAME}_loss_components.png", dpi=300)
     plt.show()
 
     
